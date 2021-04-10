8379d505b9429db0e5a4fc0bfb06a84da6c40ef6
Alejandro <ahernandezdotnet@gmail.com>
2021-04-09 20:04:38 -0500

    Reporte de inscripciones y exportacion a csv, xlsx y pdf

--- README.md
+++ README.md
@@ -3,6 +3,33 @@
 
 https://iconarchive.com/show/office-icons-by-thalita-torres/color-catalog-icon.html
 
+
+## PDF
+https://www.npmjs.com/package/jspdf
+npm install jspdf --save
+
+http://raw.githack.com/MrRio/jsPDF/master/docs/
+
+https://www.npmjs.com/package/jspdf-autotable
+npm i jspdf-autotable
+
+npm install jspdf jspdf-autotable
+
+https://github.com/simonbengtsson/jsPDF-AutoTable/tree/master/examples
+
+https://simonbengtsson.github.io/jsPDF-AutoTable/
+
+https://github.com/simonbengtsson/jsPDF-AutoTable
+
+## Excel
+https://www.npmjs.com/package/xlsx
+npm install xlsx --save
+
+## File server
+https://www.npmjs.com/package/file-saver
+npm i file-saver --save
+
+
 # GreatAdmin
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.2.2.
--- src/app/components/inscripciones/incripciones-report/incripciones-report.component.ts
+++ src/app/components/inscripciones/incripciones-report/incripciones-report.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { MessageService } from 'primeng/api';
 import { eSeverityMessages } from 'src/app/config/enums';
 import { InscripcionesService } from 'src/app/services/inscripciones.service';
-
+import jsPDF from 'jspdf'
+import autoTable from 'jspdf-autotable'
+import * as moment from 'moment';
+import { devOnlyGuardedExpression } from '@angular/compiler';
 @Component({
   selector: 'app-incripciones-report',
   templateUrl: './incripciones-report.component.html',
@@ -11,6 +14,11 @@ import { InscripcionesService } from 'src/app/services/inscripciones.service';
 export class IncripcionesReportComponent implements OnInit {
 
   inscripciones: any[] = [];
+  inscripcionesreport: any[] = [];
+  inscripcionesSelected: any[] = [];
+
+  cols: any[] = [];
+  exportColumns: any[] = [];
 
   constructor( 
     private messageService: MessageService,
@@ -19,6 +27,17 @@ export class IncripcionesReportComponent implements OnInit {
 
   ngOnInit(): void {
     this.loadInscripciones();
+
+    this.cols = [
+      { field: 'cicloescolar', header: 'Ciclo Escolar' },
+      { field: 'nivel', header: 'Nivel' },
+      { field: 'grado', header: 'Grado' },
+      { field: 'matricula', header: 'Matricula' },
+      { field: 'nombrecompleto', header: 'Nombre' }
+      ];
+
+      this.exportColumns = this.cols.map(col => ({title: col.header, dataKey: col.field}));
+
   }
 
   loadInscripciones(){
@@ -34,7 +53,17 @@ export class IncripcionesReportComponent implements OnInit {
         return;        
       }
       this.inscripciones = [ ...body.inscripciones ];
-      // this.cicloSelected = body.ciclosescolares[0];
+
+      this.inscripcionesreport = this.inscripciones.map((x) => 
+        ( 
+          { cicloescolar: x["cicloescolar"].nombre,
+            nivel: x["alumno"].nivel,
+            grado: x["alumno"].grado,
+            matricula: x["alumno"].matricula,
+            nombrecompleto: `${x["alumno"].nombre} ${x["alumno"].apaterno } ${x["alumno"].amaterno }`
+          }
+      )
+      );
     });
   }
 
@@ -46,5 +75,100 @@ export class IncripcionesReportComponent implements OnInit {
             detail: `${text}` || 'Texto'});
   }
 
+  exportPdf() {
+    let title = "Reporte de Inscripciones";
+    let footer = `Reporte generado el ${ moment(new Date() ).toString() }`;
 
+    // const doc = new jsPDF();
+    // const doc = new jsPDF("p", "px", "letter", true);
+    const doc = new jsPDF();
+    doc.setCreationDate(new Date())
+    let fonts = doc.getFontList();
+    console.log(fonts);
+    
+    const headers = [...this.cols.map( (x)=>( x.header ) )];
+
+    const pageHeight = doc.internal.pageSize.height || doc.internal.pageSize.getHeight();
+    const pageWidth = doc.internal.pageSize.width || doc.internal.pageSize.getWidth();
+
+    let textWide = doc.getStringUnitWidth(title);
+    let fontwide = doc.getFontSize();
+    let offsetX = (textWide * fontwide)/2;
+
+    var data = [ [] ];    
+    data = this.inscripcionesreport.map((x => ( Object.values(x) ) ));
+
+    console.log(headers);
+    console.log(this.inscripcionesreport);
+
+    console.log( Object.keys(fonts) );
+    console.log( Object.values(fonts) );
+
+    doc.setFont( "times", "italic", 500);
+    doc.setTextColor("navy");
+    doc.setFontSize(14);
+
+    doc.text(title, (pageWidth / 2) - title.length , 10 );
+
+    doc.setFont("Helvetica", "italic", 200);
+    doc.setTextColor("gray");
+    doc.setFontSize(8);
+
+    textWide = doc.getStringUnitWidth(footer);
+    fontwide = doc.getFontSize();
+    offsetX = (textWide * fontwide)/2;
+
+    doc.text(footer, pageWidth - offsetX, pageHeight - 10);
+
+    autoTable(doc, {
+                      // styles: { fillColor: [188, 188, 188], textColor: [0,0,0] },
+                      styles: { halign: 'center' },
+                      columnStyles: { 1: { halign: 'left' },
+                                      3: { halign: 'left'},
+                                      4: { halign: 'left'}
+                                     }, 
+                      margin: { top: 20 },
+                      head: [headers],
+                      body: data,
+                      foot: [["1", "2", "3", "4", "5"]],
+                      didDrawCell: (data) => {
+                      // console.log(data.column.index)
+                      },
+                    });
+
+    // autoTable(doc, {
+    //   styles: { fillColor: [255, 0, 0] },
+    //   columnStyles: { 0: { halign: 'center', fillColor: [0, 255, 0] } }, // Cells in first column centered and green
+    //   margin: { top: 10 },
+    //   head: headers,
+    //   body: data,
+    //   didDrawCell: (data) => {
+    //   // console.log(data.column.index)
+    //   }
+    // });
+
+
+    doc.save('table.pdf');
+
+  }
+
+  exportExcel() {
+      import("xlsx").then(xlsx => {
+          const worksheet = xlsx.utils.json_to_sheet(this.inscripcionesreport );
+          const workbook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+          const excelBuffer: any = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
+          this.saveAsExcelFile(excelBuffer, "inscripciones");
+      });
+  }
+
+  saveAsExcelFile(buffer: any, fileName: string): void {
+      import("file-saver").then(FileSaver => {
+          let EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+          let EXCEL_EXTENSION = '.xlsx';
+          const data: Blob = new Blob([buffer], {
+              type: EXCEL_TYPE
+          });
+          FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
+      });
+  }
 }
